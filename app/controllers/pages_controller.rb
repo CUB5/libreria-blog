7c695b891d7c67b2c4cda8d91af6ac71334f1be5
Javier Sanchez <javiersanchez.267@hotmail.com>
2025-02-27 20:01:22 +0100

    2h home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
 
     def index
-        images_path = Rails.root.join("app/assets/images/gallery/home")
+        images_path = Rails.root.join("app/assets/images/gallery/home/carousel1")
         images = Dir.glob("#{images_path}/*.{jpg,png,jpeg,gif}")
         @random_image = images.sample.gsub("#{Rails.root}/app/assets/images/", "")
+        
+        folder_path = Rails.root.join("app/assets/images/gallery/home/carousel2")
+        images2 = Dir.children(folder_path).select { |file| file.match?(/\.(png|jpe?g|gif|webp|svg)$/i) }
+    
+        @images_carousel= images2.map { |image| ActionController::Base.helpers.asset_path("gallery/home/carousel2/#{image}") }
+
+
     end
 
     def show 
